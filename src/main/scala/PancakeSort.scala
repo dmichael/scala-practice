fa128ccefd36a6dd35131a1880e11fcb0d5acd86
David Michael <david.michael@gmail.com>
2012-07-17 09:39:59 -0400

    Added comments to Pancake Sort

--- src/main/scala/PancakeSort.scala
+++ src/main/scala/PancakeSort.scala
@@ -1,15 +1,19 @@
 /**
  * Pancake sorting
  * ---------------
- * Pancake sorting is a variation of the sorting problem in which the only allowed operation is to reverse the elements of some prefix of the sequence. Unlike a traditional sorting algorithm, which attempts to sort with the fewest comparisons possible, the goal is to sort the sequence in as few reversals as possible. This operation can be visualized by thinking of a stack of pancakes in which one is allowed to take the top k pancakes and flip them.
+ * Pancake sorting is a variation of the sorting problem in which the only allowed operation
+ * is to reverse the elements of some prefix of the sequence. Unlike a traditional sorting algorithm,
+ * which attempts to sort with the fewest comparisons possible, the goal is to sort the sequence
+ * in as few reversals as possible. This operation can be visualized by thinking of a stack of pancakes
+ * in which one is allowed to take the top k pancakes and flip them.
  *
  * http://en.wikipedia.org/wiki/Pancake_sorting
- */
-
-/**
+ *
+ * Basic approach to the algorithm
  * 1) Find the index of the largest pancake
  * 2) Flip (reverse) the stack from the head to the largest pancake (inclusive)
- * 3) Flip the entire stack -
+ * 3) Now that the largest is at the head, separate the head from the tail and repeat with the tail (recursively)
+ * 4) Finally, the entire sorted sequence will be large to small, so flip the entire stack from the bottom.
  */
 
 class PancakeSort() {
@@ -20,13 +24,13 @@ class PancakeSort() {
     def insertAndFlip(list: List[Int]): List[Int] =  {
         if(list.isEmpty) return List()
 
-        // 1) Find the index of the largest item
+        // Find the index of the largest item
         val index = list.indexOf(list.max)
 
-        // 2) Flip (reverse) the stack from the head to the largest item (inclusive)
+        // Flip (reverse) the stack from the head to the largest item (inclusive)
         val flipped = list.slice(0, index + 1).reverse
 
-        // 3) Add the flipped portion back to the
+        // Add the flipped portion back to the stack
         val patched = list.patch(0, flipped, index + 1)
 
         val (head, tail) = patched.splitAt(1)
